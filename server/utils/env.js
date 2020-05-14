f1e135fae75c7f584774e752c86a37444fc01847
Rahulgupta4all <51700794+Rahulgupta4all@users.noreply.github.com>
2020-05-14 07:27:50 +0530

    Update env.js

--- server/utils/env.js
+++ server/utils/env.js
@@ -1,3 +1,5 @@
 module.exports = {
-    mongo: 'mongodb+srv://admin:admin@shopdb-ykzx1.mongodb.net/IssueTracker2?retryWrites=true'
+        mongo :'mongodb+srv://rahul:rahul@cluster0-rrpxn.mongodb.net/test?retryWrites=true&w=majority'
+
+    //mongo: 'mongodb+srv://admin:admin@shopdb-ykzx1.mongodb.net/IssueTracker2?retryWrites=true'
 }
